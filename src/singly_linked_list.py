ca59d2913f298422d04be203c07bf800a02a3f1f
Robert <rwallhead@gmail.com>
2015-04-19 11:18:52 +1000

    Added insert testing for singly-linked-list

--- src/singly_linked_list.py
+++ src/singly_linked_list.py
@@ -8,24 +8,55 @@ class Node(object):
     """
     def __init__(self, data=None, next_node=None):
         self.data = data
-        self.next = next_node
-
-    def __str__(self): 
-        return str(self.data)
+        self.next_node = next_node
 
 
 class SinglyLinkedList(object):
     """ The list itself.
     """
-    def __init__(self, node=None):
-        self.node = node
+    def __init__(self, first_node=None):
+        self.first_node = first_node
+
+    def __str__(self):
+        data = []
+        n = self.first_node
+        while n:
+            data.append(n.data)
+            n = n.next_node
+        return ', '.join(data)
 
     @staticmethod
     def insert_after(node, new_node):
         """ Insert a node after a given node.
 
-            Points the new_node's 'next' to the node following the given node,
-            then points the given node's 'next' to the new node.
+            Points the new_node's 'next_node' to the node following the given
+            node, then points the given node's 'next_node' to the new node.
         """
-        new_node.next = node.next
-        node.next = new_node
+        new_node.next_node = node.next_node
+        node.next_node = new_node
+
+    def insert_beginning(self, new_node):
+        """ Insert a new node at the beginning.
+
+            Point the new_node's 'next_node' to the current root node, update
+            the root node to be the new_node.
+        """
+        new_node.next_node = self.first_node
+        self.first_node = new_node
+
+    @staticmethod
+    def remove_after(node):
+        """ Remove the node after a node.
+
+            Sets the 'next_node' of the node to be the 'next_node' of the next
+            node, assuming the next node is not None.
+        """
+        if node.next_node != None:
+            node.next_node = node.next_node.next_node
+
+    def remove_beginning(self):
+        """ Remove the first node.
+
+            Sets the first_node node to be the value of the first_node's next.
+        """
+        self.first_node = self.first_node.next_node
--- src/singly_linked_list_tests.py
+++ src/singly_linked_list_tests.py
@@ -1,4 +1,6 @@
-import singly_linked_list
+""" Tests of singly-linked-lists.
+"""
+from singly_linked_list import SinglyLinkedList, Node
 import unittest
 
 
@@ -6,12 +8,45 @@ class TestSinglyLinkedLists(unittest.TestCase):
     """ Tests of singly-linked-lists.
     """
     def test_creation(self):
-        """ Test the creation of a list.
+        """ Test the creation of a list and the fundamental object attributes.
         """
-        sl_list = singly_linked_list.SinglyLinkedList()
+        sll = SinglyLinkedList()
 
-        # The list starts off with no Node - this is an empty list.
-        self.assertIs(sl_list.node, None)
+        # The list starts off with no 'first' Node - this is an empty list.
+        self.assertIs(sll.first_node, None)
+
+        # We can create a list with 'first_node' populated.
+        sll = SinglyLinkedList(Node('hello'))
+
+        self.assertEquals(sll.first_node.data, 'hello')
+
+    def test_inserts(self):
+        """ Test list inserts.
+        """
+        sll = SinglyLinkedList(Node('first'))
+
+        sll.insert_after(sll.first_node, Node('second'))
+
+        self.assertEquals(sll.first_node.data, 'first')
+
+        self.assertEquals(sll.first_node.next_node.data, 'second')
+
+        sll.insert_beginning(Node('before the first'))
+
+        self.assertEquals(sll.first_node.data, 'before the first')
+
+        self.assertEquals(sll.first_node.next_node.data, 'first')
+
+        self.assertEquals(sll.first_node.next_node.next_node.data, 'second')
+
+        self.assertEquals(str(sll), 'before the first, first, second')
+
+    def test_print(self):
+        """ Test the list printing.
+        """
+        sll = SinglyLinkedList(Node('first'))
+        sll.insert_after(sll.first_node, Node('second'))
+        self.assertEquals(str(sll), 'first, second')
 
 
 if __name__ == '__main__':
